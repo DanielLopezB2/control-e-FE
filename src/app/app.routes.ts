7dbc81d44f958f8526d8f23249cde8580d083476
Daniel Lopez <dalobe56@gmail.com>
2024-06-13 12:20:03 -0500

    Add toaster config package

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { NavContentComponent } from './layout/nav-content/nav-content.component';
+import { MainComponent } from './pages/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: '/index' },
+    {
+        path: 'index', component: NavContentComponent, children: [
+            { path: 'main', component: MainComponent },
+    ] },
+];
