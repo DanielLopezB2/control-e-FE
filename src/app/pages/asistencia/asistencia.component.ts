7bb6e1d011535dbdf4607ce2a42ec1996990b2bd
Daniel Lopez <dalobe56@gmail.com>
2024-06-17 15:02:29 -0500

    Add asistencia functionality

--- src/app/pages/asistencia/asistencia.component.ts
+++ src/app/pages/asistencia/asistencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-asistencia',
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './asistencia.component.css'
 })
 export class AsistenciaComponent {
+
+  isRegistrarAsitenciaClicked = false;
+
+  @ViewChild('checkboxes', { read: ElementRef }) checkboxesRef!: ElementRef;
+
+  registrarAsistenciaIsClicked() {
+    this.isRegistrarAsitenciaClicked = !this.isRegistrarAsitenciaClicked;
+    if (this.isRegistrarAsitenciaClicked) {
+      setTimeout(() => {
+        const tablaGrupos = document.getElementById('tabla-grupos');
+        if (tablaGrupos) {
+          tablaGrupos.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 0);
+    }
+  }
+
+  checkAll(event: Event) {
+    const checkboxes = this.checkboxesRef.nativeElement.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox: HTMLInputElement) => checkbox.checked = (event.target as HTMLInputElement).checked);
+  }  
 
 }
