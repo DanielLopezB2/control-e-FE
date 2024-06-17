e4750d7540fff66452147e7deeca77a23c7f700a
Daniel Lopez <dalobe56@gmail.com>
2024-06-17 17:57:58 -0500

    Add calificaciones functionality component

--- src/app/pages/calificaciones/calificaciones.component.ts
+++ src/app/pages/calificaciones/calificaciones.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-calificaciones',
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './calificaciones.component.css'
 })
 export class CalificacionesComponent {
+
+  isRegistrarCalificacionesClicked = false;
+
+  @ViewChild('checkboxes', { read: ElementRef }) checkboxesRef!: ElementRef;
+
+  registrarCalificacionesIsClicked() {
+    this.isRegistrarCalificacionesClicked = !this.isRegistrarCalificacionesClicked;
+    if (this.isRegistrarCalificacionesClicked) {
+      setTimeout(() => {
+        const tablaGrupos = document.getElementById('tabla-grupos');
+        if (tablaGrupos) {
+          tablaGrupos.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 0);
+    }
+  }
+
+  checkAll(event: Event) {
+    const checkboxes = this.checkboxesRef.nativeElement.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox: HTMLInputElement) => checkbox.checked = (event.target as HTMLInputElement).checked);
+  }  
 
 }
